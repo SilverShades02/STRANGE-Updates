c8c817b686bed6b65557d9aa867a4b47742ffab3
Jyotiraditya <jyotiraditya182@gmail.com>
2018-11-03 08:50:33 +0000

    Initial Santoni (#1)
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

---
+++ santoni.php
@@ -0,0 +1,1 @@
+{"updates":[{"channel":"STRANGE","name":"aoscp_santoni-7.0.0-20181103-STRANGE.zip","version":"7.0.0","build":"20181103","size":"700732116","url":".zip","md5":"a0a9db6568015363a3b119b2b91d1476","text":"U Sadap...."}]}
